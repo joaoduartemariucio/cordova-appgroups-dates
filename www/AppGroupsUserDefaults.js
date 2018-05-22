0e7085b39d56e5d86da848bc814af0b978d5fdd3
Joao Duarte <31930009+joaoduartemariucio@users.noreply.github.com>
2018-05-22 09:36:06 -0300

    all back

--- www/AppGroupsUserDefaults.js
+++ www/AppGroupsUserDefaults.js
@@ -8,9 +8,5 @@ AppGroupsUserDefaults.prototype.load = function(options, success, fail) {
   cordova.exec(success, fail, "AppGroupsUserDefaults", "load", [options]);
 };
 
-AppGroupsUserDefaults.prototype.clearAll = function(success, fail) {
-  cordova.exec(success, fail, "AppGroupsUserDefaults", "clearAll");
-};
-
 var appGroupsUserDefaults = new AppGroupsUserDefaults();
 module.exports = appGroupsUserDefaults;
